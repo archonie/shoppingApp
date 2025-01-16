3c10e0f4cd769e852ccf671c997ed6b5f409f72f
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-17 01:21:59 +0300

    Initial Commit

---
+++ ShoppingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 17.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
