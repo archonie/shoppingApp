eb1fda6948747cccf7a3a2d598d25564995ebd9b
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-18 20:06:52 +0300

    Create ProductView and CartButton

---
+++ ShoppingApp/Components/CartButton.swift
@@ -0,0 +1,30 @@
+//
+//  CartButton.swift
+//  ShoppingApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 18.01.2025.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "bag.fill")
+                .padding(5)
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2)
+                    .foregroundStyle(.white)
+                    .frame(width: 15, height: 15)
+                    .background(.green)
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+#Preview {
+    CartButton(numberOfProducts: 1)
+}
---
+++ ShoppingApp/Views/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  ShoppingApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 18.01.2025.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
